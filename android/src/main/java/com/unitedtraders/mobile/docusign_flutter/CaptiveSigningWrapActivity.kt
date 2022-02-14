37af62c71481ccaaa5daa473a7d3faf254469f26
v.smirnov <v.smirnov@unitedtraders.com>
2022-02-14 18:22:27 +0300

    FLTR-775 return android code, add prod environment, fix bug

--- android/src/main/java/com/unitedtraders/mobile/docusign_flutter/CaptiveSigningWrapActivity.kt
+++ android/src/main/java/com/unitedtraders/mobile/docusign_flutter/CaptiveSigningWrapActivity.kt
@@ -2,24 +2,81 @@ package com.unitedtraders.mobile.docusign_flutter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.docusign.androidsdk.DocuSign
+import com.docusign.androidsdk.exceptions.DSSigningException
+import com.docusign.androidsdk.listeners.DSCaptiveSigningListener
+import com.google.gson.Gson
 
 
 class CaptiveSigningWrapActivity : AppCompatActivity() {
-    companion object {
-        private var firstRun: Boolean = true
-    }
+    private var firstRun: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wrap)
 
-        if (!firstRun) {
+        if (firstRun) {
             firstRun = false
             startCaptiveSigning()
         }
     }
 
     private fun startCaptiveSigning() {
+        val result = DataBrokerSingleton.instance.captiveSigningResult ?: return
 
+        val call = DataBrokerSingleton.instance.captiveSigningCall
+        if (call == null) {
+            result.error(Constants.errorCommonCode, Constants.errorCaptiveSingingFailed, "CaptiveSigningCall is null")
+            return
+        }
+
+        val params: DocusignFlutterPlugin.CaptiveSigningModel
+        try {
+            @Suppress("UNCHECKED_CAST")
+            val arguments: ArrayList<String> = call.arguments as ArrayList<String>
+            val json: String = arguments[0]
+            val gson = Gson()
+            params = gson.fromJson(json, DocusignFlutterPlugin.CaptiveSigningModel::class.java)
+        } catch (e: Exception) {
+            result.error(Constants.errorCommonCode, Constants.errorCaptiveSingingFailed, "Cannot parse CaptiveSigningModel json: ${e.message}")
+            return
+        }
+
+        try {
+            DocuSign.getInstance().getSigningDelegate().launchCaptiveSigning(this,
+                envelopeId = params.envelopeId,
+                recipientClientUserId = params.recipientClientUserId,
+                listener = object : DSCaptiveSigningListener {
+                    override fun onCancel(envelopeId: String, recipientId: String) {
+                        result.error(Constants.errorCommonCode, Constants.errorCaptiveSingingFailed, "User cancel captive signing")
+                        finish()
+                    }
+
+                    override fun onError(envelopeId: String?, exception: DSSigningException) {
+                        result.error(Constants.errorCommonCode, Constants.errorCaptiveSingingFailed, exception.message)
+                        finish()
+                    }
+
+                    override fun onRecipientSigningError(
+                        envelopeId: String,
+                        recipientId: String,
+                        exception: DSSigningException
+                    ) {
+                    }
+
+                    override fun onRecipientSigningSuccess(envelopeId: String, recipientId: String) {
+                    }
+
+                    override fun onStart(envelopeId: String) {
+                    }
+
+                    override fun onSuccess(envelopeId: String) {
+                        result.success(null)
+                        finish()
+                    }
+                })
+        } catch (e: Exception) {
+            result.error(Constants.errorCommonCode, Constants.errorCaptiveSingingFailed, e.message)
+        }
     }
 }
--- android/src/main/kotlin/com/unitedtraders/mobile/docusign_flutter/DocusignFlutterPlugin.kt
+++ android/src/main/kotlin/com/unitedtraders/mobile/docusign_flutter/DocusignFlutterPlugin.kt
@@ -2,7 +2,11 @@ package com.unitedtraders.mobile.docusign_flutter
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat.startActivity
+import com.docusign.androidsdk.DSEnvironment
+import com.docusign.androidsdk.DocuSign
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
@@ -11,6 +15,11 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import com.docusign.androidsdk.dsmodels.DSUser
+import com.docusign.androidsdk.exceptions.DSAuthenticationException
+import com.docusign.androidsdk.listeners.DSAuthenticationListener
+import com.docusign.androidsdk.util.DSMode
+import com.google.gson.Gson
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 
@@ -36,11 +45,58 @@ class DocusignFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   }
 
   private fun login(@NonNull call: MethodCall, @NonNull result: Result) {
-    result.notImplemented()
+    val params: AuthModel
+    try {
+      @Suppress("UNCHECKED_CAST")
+      val arguments: ArrayList<String> = call.arguments as ArrayList<String>
+      val json: String = arguments[0]
+      val gson = Gson()
+      params = gson.fromJson(json, AuthModel::class.java)
+    } catch (e: Exception) {
+      result.error(Constants.errorCommonCode, Constants.errorAuthFailed, "Cannot parse AuthModel json: ${e.message}")
+      return
+    }
+
+    try {
+      val isDemo = params.host.indexOf("demo") != -1
+      val dsMode = if (isDemo) DSMode.DEBUG else DSMode.PRODUCTION
+      val dsEnvironment = if (isDemo) DSEnvironment.DEMO_ENVIRONMENT else DSEnvironment.PRODUCTION_ENVIRONMENT
+      DocuSign.init(context, integratorKey = params.integratorKey, mode = dsMode)
+      DocuSign.getInstance().setEnvironment(dsEnvironment)
+      val authDelegate = DocuSign.getInstance().getAuthenticationDelegate()
+      authDelegate.login(
+        accessToken = params.accessToken,
+        refreshToken = null,
+        expiresIn = params.expiresIn,
+        context,
+        object : DSAuthenticationListener {
+          override fun onSuccess(@NonNull user: DSUser) {
+            result.success(null)
+          }
+
+          override fun onError(@NonNull exception: DSAuthenticationException) {
+            result.error(Constants.errorCommonCode, Constants.errorAuthFailed, exception.message)
+          }
+        },
+        userAccountId = params.accountId,
+      )
+    } catch (e: Exception) {
+      result.error(Constants.errorCommonCode, Constants.errorAuthFailed, e.message)
+      return
+    }
   }
 
   private fun captiveSinging(@NonNull call: MethodCall, @NonNull result: Result) {
-    result.notImplemented()
+    try {
+      DataBrokerSingleton.instance.captiveSigningResult = result
+      DataBrokerSingleton.instance.captiveSigningCall = call
+
+      val intent = Intent(context, CaptiveSigningWrapActivity::class.java)
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+      startActivity(context, intent, null)
+    } catch (e: Exception) {
+      result.error(Constants.errorCommonCode, Constants.errorCaptiveSingingFailed, "Cannot start wrap activity")
+    }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
